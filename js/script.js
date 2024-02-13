8a467405097ec81f82d4e04c923c745fcc407f4f
ChristopherMonteleone <christophergmonteleone@gmail.com>
2024-02-13 15:21:45 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var toggleButtons = ['toggle-button1', 'toggle-button2', 'toggle-button3'];
-    var toggleDivs = ['toggle-div1', 'toggle-div2', 'toggle-div3'];
+    var toggleButtons = ['toggle-button1', 'toggle-button2', 'toggle-button3', 'toggle-button4', 'toggle-button5', 'toggle-button6', 'toggle-button7', 'toggle-button8', 'toggle-button9'];
+    var toggleDivs = ['toggle-div1', 'toggle-div2', 'toggle-div3', 'toggle-div4', 'toggle-div5', 'toggle-div6', 'toggle-div7', 'toggle-div8', 'toggle-div9'];
 
     function hideAllDivs() {
         toggleDivs.forEach(function(divId) {
